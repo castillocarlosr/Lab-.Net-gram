7f9dd868f2852d55a49557a67cfe44a5070173fb
castillocarlosr <castillocarlosr@gmail.com>
2019-02-13 13:48:59 -0800

    Made Management classes

---
+++ NetGram/NetGram/Pages/Work/Manage.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NetGram.Models;
+using NetGram.Models.Interfaces;
+
+namespace NetGram.Pages.Work
+{
+    public class ManageModel : PageModel
+    {
+        private readonly IWorkGram _workGram;
+
+        [FromRoute]
+        public int? ID { get; set; }
+
+        public ManageModel(IWorkGram workGram)
+        {
+            _workGram = workGram;
+        }
+
+        public async Task OnGet()
+        {
+            WorkGram = await _workGram.FindWorkGram(ID.GetValueOrDefault()) ?? new WorkGram();
+        }
+    }
+}
