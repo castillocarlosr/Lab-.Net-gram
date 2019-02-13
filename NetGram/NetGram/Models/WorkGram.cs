82d42b88b8971b5193e6bed88d60dd4ea6d1c89b
castillocarlosr <castillocarlosr@gmail.com>
2019-02-13 09:58:34 -0800

    interface setup

---
+++ NetGram/NetGram/Models/WorkGram.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetGram.Models
+{
+    public class WorkGram
+    {
+        public int ID { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Title { get; set; }
+
+        public string URL { get; set; }
+
+        public string Comments { get; set; }
+
+        public int Views { get; set; }
+    }
+}
