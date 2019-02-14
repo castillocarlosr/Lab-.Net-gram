87848498b864e1ad90c02b99d8fd7ba20f9681a2
castillocarlosr <castillocarlosr@gmail.com>
2019-02-14 10:10:57 -0800

    seed image holder

--- NetGram/NetGram/Data/WorkGramDbContext.cs
+++ NetGram/NetGram/Data/WorkGramDbContext.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static System.Net.WebRequestMethods;
 
 namespace NetGram.Data
 {
@@ -31,7 +32,7 @@ namespace NetGram.Data
                     ID = 2,
                     UserName = "User Doc",
                     Title = "Doctor in office",
-                    URL = "/assets/pic002.jpg",
+                    URL = "../assets/pic002.jpg",
                     Comments = "Making you healthy",
                     Views = 1
                 },
@@ -40,7 +41,7 @@ namespace NetGram.Data
                     ID = 3,
                     UserName = "Mobile Lab",
                     Title = "Dog Coding",
-                    URL = "/assetst/pic003.JPG",
+                    URL = "../../assetst/pic003.JPG",
                     Comments = "I do odd jobs",
                     Views = 1
                 },
@@ -49,7 +50,7 @@ namespace NetGram.Data
                     ID = 4,
                     UserName = "OctoCat",
                     Title = "Teaching Code",
-                    URL = "/assets/pic004.jpg",
+                    URL = "assets/pic004.jpg",
                     Comments = "I watch while you code.",
                     Views = 1
                 },
@@ -58,7 +59,7 @@ namespace NetGram.Data
                     ID = 5,
                     UserName = "SeattleChemist",
                     Title = "Hood Time",
-                    URL = "/assets/pic005.JPG",
+                    URL = "pic005.JPG",
                     Comments = "Such a reaction.",
                     Views = 1
                 },
@@ -67,8 +68,26 @@ namespace NetGram.Data
                     ID = 6,
                     UserName = "Hot Dog Champ",
                     Title = "Winning 1st",
-                    URL = "C:/Users/casti/coding/codeFellows/code401/Lab-.Net-gram/NetGram/NetGram/wwwroot/Assets/pic006.jpg",
+                    URL = "https://via.placeholder.com/150/0000FF/808080 ?Text=Place Holder",
                     Comments = "I get paid to eat hot dogs.",
+                    Views = 1
+                },
+                new WorkGram
+                {
+                    ID = 7,
+                    UserName = "number 7",
+                    Title = "Losing 1st",
+                    URL = "https://via.placeholder.com/150",
+                    Comments = "7th place",
+                    Views = 1
+                },
+                new WorkGram
+                {
+                    ID = 8,
+                    UserName = "Dino",
+                    Title = "Winning always",
+                    URL = "pic006.JPG",
+                    Comments = "ROAR.",
                     Views = 1
                 }
                 );
